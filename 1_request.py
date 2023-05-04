622336e50d27891a18ed888026b7f29b41f2bb12
anaxavierr <129238533+anaxavierr@users.noreply.github.com>
2023-05-04 11:45:59 -0300

    Add files via upload

--- 1_request.py
+++ 1_request.py
@@ -1,7 +1,10 @@
+# Importando as bibliotecas necessárias
 import requests
 import pandas as pd
 
+# Definindo a função de busca de podcasts do Spotify
 def search_spotify_podcasts(query):
+    # Definindo a URL base e os parâmetros da API do Spotify
     base_url = 'https://api.spotify.com/v1/search'
     params = {
         'q': query,
@@ -9,39 +12,57 @@ def search_spotify_podcasts(query):
         'market': 'BR',
         'limit': 50
     }
+
+    # Definindo as credenciais de autenticação da API do Spotify
     client_id = '58f1d9a3438d4148b3835418d78c780f'
     client_secret = '665ae28bb8f74c28bc6b349ae0e966cb'
     auth_url = 'https://accounts.spotify.com/api/token'
+
+    # Realizando a autenticação e obtendo o token de acesso
     auth_response = requests.post(auth_url, {
         'grant_type': 'client_credentials',
         'client_id': client_id,
         'client_secret': client_secret,
     })
     access_token = auth_response.json()['access_token']
+
+    # Definindo o cabeçalho de autenticação para as requisições à API do Spotify
     headers = {
         'Authorization': f'Bearer {access_token}'
     }
+
+    # Realizando a requisição à API do Spotify e obtendo os resultados
     response = requests.get(base_url, headers=headers, params=params)
     data = response.json()
     items = data.get('shows', {}).get('items', [])
+
+    # Criando uma lista para armazenar os dados de cada podcast e iterando sobre os resultados
     table = []
     print(f"Total items: {len(items)}")
     for item in items:
         if not item:
             continue
+
+        # Obtendo os dados de cada podcast
         name = item.get('name', '')
         description = item.get('description', '')
         podcast_id = item.get('id', '')
         total_episodes = item.get('total_episodes', '')
+
+        # Armazenando os dados de cada podcast na lista
         table.append({
             'name': name,
             'description': description,
             'id': podcast_id,
             'total_episodes': total_episodes
         })
+
+    # Convertendo a lista de dados em um DataFrame do Pandas e salvando como um arquivo CSV
     df = pd.DataFrame(table, columns=['name', 'description', 'id', 'total_episodes'])
     df.to_csv('podcasts.csv', index=False)
+
+    # Retornando o DataFrame com os dados dos podcasts
     return df
 
-# Exemplo de uso
+# Uso da função de busca de podcasts do Spotify
 podcast_df = search_spotify_podcasts('data hackers')
--- 2_dados.py
+++ 2_dados.py
@@ -1,11 +1,21 @@
+# Importando as bibliotecas necessárias
 import requests
 import pandas as pd
 
+# Definindo a função de busca de podcasts do Spotify
 def get_datahackers_episodes():
-    podcast_id = '1oMIHOXsrLFENAeM743g93'
+
+    # ID do podcast DataHackers no Spotify
+    podcast_id = '1oMIHOXsrLFENAeM743g93' 
+
+    # URL da API do Spotify para obter informações dos episódios do podcast
     episodes_url = f"https://api.spotify.com/v1/shows/{podcast_id}/episodes"
+
+    # Credenciais de acesso à API do Spotify
     client_id = '58f1d9a3438d4148b3835418d78c780f'
     client_secret = '665ae28bb8f74c28bc6b349ae0e966cb'
+
+    # URL para autenticação e obtenção do token de acesso à API do Spotify
     auth_url = 'https://accounts.spotify.com/api/token'
     auth_response = requests.post(auth_url, {
         'grant_type': 'client_credentials',
@@ -13,6 +23,8 @@ def get_datahackers_episodes():
         'client_secret': client_secret,
     })
     access_token = auth_response.json()['access_token']
+
+    # Headers para a requisição à API do Spotify com o token de acesso
     headers = {
         'Authorization': f'Bearer {access_token}'
     }
@@ -20,16 +32,24 @@ def get_datahackers_episodes():
     limit = 50
     offset = 0
     total = 1
+
+    # Loop para obter as informações de todos os episódios
     while offset < total:
+
+        # Parâmetros da requisição à API do Spotify
         params = {
             'type': 'episode',
             'market': 'BR',
             'limit': limit,
             'offset': offset
         }
+
+        # Requisição à API do Spotify para obter informações dos episódios
         response = requests.get(episodes_url, headers=headers, params=params)
         data = response.json()
         episodes = data.get('items', [])
+
+        # Loop para obter as informações de cada episódio obtido na requisição
         for episode in episodes:
             name = episode.get('name', '')
             description = episode.get('description', '')
@@ -43,12 +63,20 @@ def get_datahackers_episodes():
                 'release_date': release_date,
                 'duration_ms': duration_ms
             })
+
+        # Atualização do total de episódios e do offset para a próxima requisição
         total = data.get('total', 0)
         offset += limit
+
+    # Criação de um dataframe com as informações de todos os episódios
     df = pd.DataFrame(all_episodes, columns=['name', 'description', 'id', 'release_date', 'duration_ms'])
+
+    # Exportação do dataframe para um arquivo csv
     df.to_csv('datahackers_episodes.csv', index=False)
+
+    # Exibição do número total de episódios encontrados
     print(f"Total de episódios encontrados: {len(all_episodes)}")
     return df
 
-# Exemplo de uso
+# Uso da função
 datahackers_episodes_df = get_datahackers_episodes()
